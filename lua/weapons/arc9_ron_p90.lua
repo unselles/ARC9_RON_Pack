5da9f5757bfbfbac70982eed928a16ce8877aded
unselles <pumape2@seznam.cz>
2023-06-07 02:53:10 +0200

    Better firing anim, lua changes

--- lua/weapons/arc9_ron_p90.lua
+++ lua/weapons/arc9_ron_p90.lua
@@ -184,7 +184,7 @@ SWEP.CrouchPos				= Vector(-4, -12, -1)
 SWEP.CrouchAng				= Angle(0, 0, -10)
 
 SWEP.CustomizeAng			= Angle(90, 0, 0)
-SWEP.CustomizePos			= Vector(20, 10, 5)
+SWEP.CustomizePos			= Vector(20, 25, 5)
 SWEP.CustomizeSnapshotFOV	= 70
 
 -------------------------- HoldTypes
